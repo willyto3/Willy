acb4f9c476c606d0c089b08247bd2233fa0d7777
Willy Corzo <ing.willy.corzo@gmail.com>
2021-06-13 12:16:51 -0500

    Co-authored-by: rafapua89 <rafapua89@users.noreply.github.com> Co-authored-by: Jorge Santander <Kikecarp8721@users.noreply.github.com>

--- reto5.py
+++ reto5.py
@@ -7,16 +7,16 @@ import math
 import os
 
 # borramos el terminal
-#os.system("cls")
+os.system("cls")
 
 # DECLARAMOS LAS VARIABLES
-n = 0
+n = 33
 antenas_iniciales = -1    
 lista_antenas= []
 antenas_totales = 0
 numero_de_antenas = 0
 
-"""
+
 # Funciones
 # Funcion para calcular el numero de antenas recibe 3 variables
 # (Area de Zona, Antenas Iniciales y Alcance de antena nueva) y retorna el numero de antena
@@ -29,7 +29,7 @@ def nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva):
         numero_de_antenas =0
     return numero_de_antenas
 
-
+"""
 # Funcion para buscar la letra (tipo de antena nueva) en función de la posición de la lista_antenas, recibe una variable.
 # (Indice) y retorna letra (tipo de antena)
 
@@ -48,13 +48,20 @@ def buscarletra(indice):
 
 """
 
-# Solicitamos los departamentos a buscar (n)
+# Solicitamos los departamentos a buscar (n) y los guardamos en una lista
 
-departamentos= input().split(" ")
+lista_departamentos= input().split(" ")
 
-# Creamos una matriz de departamentos filas (numero de departamento) por 6 columnas (tipo de antenas)
+lista_departamentos.sort()
 
-# comentario
+# Creamos una matriz de departamentos filas (numero de departamento) por 5 columnas (tipo de antenas)
+matriz_departamentos=[]
+
+for fila in range(len(lista_departamentos)):
+  matriz_departamentos.append([])
+  for columna in range(5):
+    matriz_departamentos[fila].append(0)
+
 
 # Elegimos la Ruta del archivo CSV
 ruta = "data.csv"
@@ -67,99 +74,81 @@ lineas = archivo.readlines()
 matriz = []
 # Ciclo para recorrer las lineas sin contar la linea 0
 for i in range(1, len(lineas)):
-    matriz.append(lineas[i].split(","))
+  matriz.append(lineas[i].split(","))
 
 # Variable de conteo
-for departamento in departamentos:
-
+for departamento in lista_departamentos:
+  indice=lista_departamentos.index(departamento)
   numero_departamento = departamento
   nombre_departamento = ''
   
-  # Ciclo de conteo
+
   for fila in matriz:
       if fila[0] == numero_departamento:
-          nombre_departamento = fila[1]
-          area_de_zona = float(fila[2])
-          antenas_iniciales = int(fila[3])
-          tipo_antenas_nuevas = fila[4].replace("\n", "")
+        nombre_departamento = fila[1]
+        area_de_zona = float(fila[2])
+        antenas_iniciales = int(fila[3])
+        tipo_antenas_nuevas = fila[4].replace("\n", "")
 
-# Creamos una lista con el numero de antenas en cada departamento y lo iniciamos en cero
-for i in range(n):
-  lista_departamentos.append(0)
-
-# Se llena la lista de antenas con sublistas (filas) correspondientes al tipo de antenas (a,b,c,d,e un total de 5 posiciones)
-for i in range(5):
-  lista_antenas.append([])
-  # Se llena cada sublista con el numero de departamentos "n" (columnas)
-  for j in range(n):
-    lista_antenas[i].append(0)
-
-# ENTRADAS
-# Realizamos un ciclo for por cada una de las zonas o terrenos
-for i in range(m):
-    # Se reinicia los contadores de las antenas.
-    antenas_a = 0
-    antenas_b = 0
-    antenas_c = 0
-    antenas_d = 0
-    antenas_e = 0
-
-    # Realizamos un ciclo while mientras que las antenas iniciales sean menores de 0, por esta razón antenas_iniciales comienzan valiendo -1.
-    while antenas_iniciales < 0:
-      # Solicitamos numero de departamento, area de zona, antenas previamente instaladas y el tipo de antena a instalar
-      numero_departamento,area_de_zona,antenas_iniciales,tipo_antenas_nuevas = input().split(" ") 
-      numero_departamento = int(numero_departamento) 
-      area_de_zona = float(area_de_zona)
-      antenas_iniciales = int(antenas_iniciales)
+        # ENTRADAS
+        # Se reinicia los contadores de las antenas.
+        antenas_a = 0
+        antenas_b = 0
+        antenas_c = 0
+        antenas_d = 0
+        antenas_e = 0
       
-      if numero_departamento > n:
-        numero_departamento = False
+        if int(numero_departamento) > n:
+          numero_departamento = False
 
-      # Verificacion de tipo de antena a Instalar
-      if tipo_antenas_nuevas == "a" and int(antenas_iniciales) >= 0:
-        alcance_antena_nueva = 33_600
-        # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
-        antenas_a = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
-        # Guardamos el dato de antenas "a" en la posición correspondiente de la lista_antenas
-        lista_antenas[0][numero_departamento-1] = antenas_a + \
-            lista_antenas[0][numero_departamento-1]
+        # Verificacion de tipo de antena a Instalar
+        if tipo_antenas_nuevas == "a" and int(antenas_iniciales) >= 0:
+          alcance_antena_nueva = 33_600
+          # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
+          antenas_a = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
+          # Guardamos el dato de antenas "a" en la posición correspondiente de la lista_antenas
+          matriz_departamentos[indice][0] = antenas_a + \
+              matriz_departamentos[indice][0]
 
-      elif tipo_antenas_nuevas == "b" and int(antenas_iniciales) >= 0:
-        alcance_antena_nueva = 48_900
-        # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
-        antenas_b= nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
-        # Guardamos el dato de antenas "b" en la posición correspondiente de la lista_antenas
-        lista_antenas[1][numero_departamento-1] = antenas_b + \
-            lista_antenas[1][numero_departamento-1]
-            
-      elif tipo_antenas_nuevas == "c" and int(antenas_iniciales) >= 0:
-        alcance_antena_nueva = 17_000
-        # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
-        antenas_c = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
-        # Guardamos el dato de antenas "c" en la posición correspondiente de la lista_antenas
-        lista_antenas[2][numero_departamento-1] = antenas_c + \
-            lista_antenas[2][numero_departamento-1]
-            
-      elif tipo_antenas_nuevas == "d" and int(antenas_iniciales) >= 0:
-        alcance_antena_nueva = 21_000
-        # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
-        antenas_d = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
-        # Guardamos el dato de antenas "d" en la posición correspondiente de la lista_antenas
-        lista_antenas[3][numero_departamento-1] = antenas_d + \
-            lista_antenas[3][numero_departamento-1]
-            
-      elif tipo_antenas_nuevas == "e" and int(antenas_iniciales) >= 0:
-        alcance_antena_nueva = 42_600
-        # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
-        antenas_e = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
-        # Guardamos el dato de antenas "e" en la posición correspondiente de la lista_antenas
-        lista_antenas[4][numero_departamento-1] = antenas_e + \
-            lista_antenas[4][numero_departamento-1]
-            
-      else:
-        # Si es un tipo de antena no definido dentro del tipo de antenas (a,b,c,d,e) el tipo de antenas lo coloca como falso
-        tipo_antenas_nuevas = False
-    
+        elif tipo_antenas_nuevas == "b" and int(antenas_iniciales) >= 0:
+          alcance_antena_nueva = 48_900
+          # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
+          antenas_b= nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
+          # Guardamos el dato de antenas "b" en la posición correspondiente de la lista_antenas
+          matriz_departamentos[indice][1] = antenas_b + \
+              matriz_departamentos[indice][1]
+              
+        elif tipo_antenas_nuevas == "c" and int(antenas_iniciales) >= 0:
+          alcance_antena_nueva = 17_000
+          # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
+          antenas_c = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
+          # Guardamos el dato de antenas "c" en la posición correspondiente de la lista_antenas
+          matriz_departamentos[indice][2] = antenas_c + \
+              matriz_departamentos[indice][2]
+              
+        elif tipo_antenas_nuevas == "d" and int(antenas_iniciales) >= 0:
+          alcance_antena_nueva = 21_000
+          # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
+          antenas_d = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
+          # Guardamos el dato de antenas "d" en la posición correspondiente de la lista_antenas
+          matriz_departamentos[indice][3] = antenas_d + \
+              matriz_departamentos[indice][3]
+              
+        elif tipo_antenas_nuevas == "e" and int(antenas_iniciales) >= 0:
+          alcance_antena_nueva = 42_600
+          # Ejecuta la funcion nantenas, para hallar el numero de antenas a instalar
+          antenas_e = nantenas(area_de_zona, antenas_iniciales, alcance_antena_nueva)
+          # Guardamos el dato de antenas "e" en la posición correspondiente de la lista_antenas
+          matriz_departamentos[indice][4] = antenas_e + \
+              matriz_departamentos[indice][4]
+              
+        else:
+          # Si es un tipo de antena no definido dentro del tipo de antenas (a,b,c,d,e) el tipo de antenas lo coloca como falso
+          tipo_antenas_nuevas = False
+
+print(matriz_departamentos)
+
+"""  
     # Comprobamos que  tipo de antenas nuevas y numero de departamento correspondan a los tipos establecidos
     if tipo_antenas_nuevas != False and numero_departamento != False:
       # Se calculan las antenas totales requeridas
@@ -228,18 +217,18 @@ for fila in range(5):
   # Se imprime la sexta salida que pide el ejercicio: 
   # Halla El número del departamento con mayor cantidad de antenas instaladas del tipo correspondiente, seguido del tipo de la nueva antena y el valor de las nuevas antenas instaladas de ese tipo.
   print(lista.index(mayor)+1, buscarletra(fila), mayor)
-"""
 
 
 
-"""
+
+
 # Funcion para impresion
 # de arreglo bidimensional
 def imprimir(a):
     for item in a:
         print(item)
     print()
-  """
+  
 
 """
 """          
