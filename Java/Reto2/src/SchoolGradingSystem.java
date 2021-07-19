a601fb241c5e36128229ce9d8a3e31b929e2f099
Willy Corzo <ing.willy.corzo@gmail.com>
2021-07-19 02:30:00 -0500

    Se terminan con los comentarios

--- Java/Reto2/src/SchoolGradingSystem.java
+++ Java/Reto2/src/SchoolGradingSystem.java
@@ -1,18 +1,26 @@
+
+// Se declaran las importaciones
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// Se utiliza el metodo de camello para declarar todas las variables ejemplo unaVariable
+
 // Se debe implementar todo el procesamiento de información y cálculos en una clase llamada SchoolGradingSystem.
 public class SchoolGradingSystem {
+    // se declaran las variables
     private float nombre;
     private float genero;
     private float materia;
     private float calificacion;
-    private static List<SchoolGradingSystem> desempeño_de_estudiantes = new ArrayList<>();
+    // Declaracion de la lista desempeño de estudiantes para guardar los datos
+    private static List<SchoolGradingSystem> desempeñoDeEstudiantes = new ArrayList<>();
 
+    // Se genera el constructor Vacio
     public SchoolGradingSystem() {
     }
 
+    // Se genera el contructor de las variables
     public SchoolGradingSystem(float nombre, float genero, float materia, float calificacion) {
         this.nombre = nombre;
         this.genero = genero;
@@ -20,137 +28,182 @@ public class SchoolGradingSystem {
         this.calificacion = calificacion;
     }
 
+    // Metodo get de la variable nombre
     public float getNombre() {
         return nombre;
     }
 
+    // Metodo set de la variable nombre
     public void setNombre(float nombre) {
         this.nombre = nombre;
     }
 
+    // Metodo get de la variable genero
     public float getGenero() {
         return genero;
     }
 
+    // Metodo set de la variable genero
     public void setGenero(float genero) {
         this.genero = genero;
     }
 
+    // Metodo get de la variable materia
     public float getMateria() {
         return materia;
     }
 
+    // Metodo set de la variable materia
     public void setMateria(float materia) {
         this.materia = materia;
     }
 
+    // Metodo get de la variable calificacion
     public float getCalificacion() {
         return calificacion;
     }
 
+    // Metodo set de la variable calificacion
     public void setCalificacion(float calificacion) {
         this.calificacion = calificacion;
     }
 
+    // La clase SchoolGradingSystem debe contener un método llamado readData,
+    // el cual no debe recibir ni retornar ningún parámetro.
+    // El objetivo de este método es leer los valores de entrada del programa.
     public static void readData() {
+        // Se declara el objeto estudiante de la clase SchoolGradingSystem
         SchoolGradingSystem estudiante;
+        // Se genera el Scanner entrada para recopilar los datos
         Scanner entrada = new Scanner(System.in);
 
-        // Generamos las entradas al programa:número de estudiantes al programa
-        int numero_de_estudiantes = entrada.nextInt();
+        // Generamos las entradas al programa:número de calificaciones
+        int numeroDeCalificaciones = entrada.nextInt();
 
-        // Generamos la matriz de [número_de_estudiantes][3]
-        // float desempeño_de_estudiantes[][] = new float[numero_de_estudiantes][4];
-
-        // Usamos el ciclo for para alimentar la matriz desempeño_de_estudiantes
-        for (int fila = 0; fila < numero_de_estudiantes; fila++) {
-            // Generamos la lista de entradas: nombre, genero, matería y calificación
-            // System.out.println("Ingrese el nombre, genero, matería y calificación.
-            // Separados por espacio");
+        // Usamos el ciclo for para solicitar los datos
+        for (int fila = 0; fila < numeroDeCalificaciones; fila++) {
             float nombre = entrada.nextFloat();
             float genero = entrada.nextFloat();
             float materia = entrada.nextFloat();
             float calificacion = entrada.nextFloat();
 
+            // creamos un objeto de la clase SchoolGradingSystem
             estudiante = new SchoolGradingSystem(nombre, genero, materia, calificacion);
-            desempeño_de_estudiantes.add(estudiante);
+            // Añadimos el objeto creado a la lista de objetos de la clase
+            // SchoolGradingSystem
+            desempeñoDeEstudiantes.add(estudiante);
 
         }
 
     }
 
+    // La clase debe contener 4 métodos, estos métodos no deben recibir ningún
+    // parámetro y la salida de estos métodos debe ser el valor que da respuesta a
+    // cada uno de las preguntas. Los métodos deben ser nombrados question#, donde #
+    // corresponde al número de la pregunta a la cual debe dar respuesta, para este
+    // caso serían los números del 1 al 4
+
+    // Realizamos el metodo question1 Notas inferiores al promedio
     public static void question1() {
 
-        float sumatoria_de_notas = 0;
-        // Calculamos el promedio de las notas, iniciando sumatoria_de_notas=0
+        // Declaramos un acumulador de notas
+        float sumatoriaDeNotas = 0;
 
-        for (int i = 0; i < desempeño_de_estudiantes.size(); i++) {
-            // System.out.println(desempeño_de_estudiantes.get(i).getCalificacion());
-            sumatoria_de_notas += desempeño_de_estudiantes.get(i).getCalificacion();
+        // Calculamos el promedio de las notas. recorriendo la lista desempeño de
+        // estudiantes
+        for (int i = 0; i < desempeñoDeEstudiantes.size(); i++) {
+            sumatoriaDeNotas += desempeñoDeEstudiantes.get(i).getCalificacion();
         }
 
-        float promedio_calificaciones = sumatoria_de_notas / desempeño_de_estudiantes.size();
+        // Calculamos el promedio de las calificaciones division de la sumatoria de
+        // notas entre el tamaño de la lista desempeño de estudiantes
+        float promedioCalificaciones = sumatoriaDeNotas / desempeñoDeEstudiantes.size();
 
-        // Se contabiliza el número de notas inferiores al promedio de notas
-        int notas_inferiores_a_promedio = 0;
+        // Declaramos un contador de notas inferiores al promedio
+        int notasInferioresAPromedio = 0;
 
-        for (int i = 0; i < desempeño_de_estudiantes.size(); i++) {
-            // sumatoria_de_notas += desempeño_de_estudiantes.get(i).getCalificacion();
-            if (desempeño_de_estudiantes.get(i).getCalificacion() < promedio_calificaciones) {
-                notas_inferiores_a_promedio += 1;
+        // Recorremos la lista desempeño de estudiantes para comparar el promedio de
+        // notas con cada una de las notas de la lista
+        for (int i = 0; i < desempeñoDeEstudiantes.size(); i++) {
+            // Si la nota es inferior al promedio la contamos en nuestro contador notas
+            // inferiores al promedio
+            if (desempeñoDeEstudiantes.get(i).getCalificacion() < promedioCalificaciones) {
+                notasInferioresAPromedio += 1;
             }
 
         }
-
-        System.out.println("Notas inferiores al promedio " + notas_inferiores_a_promedio);
+        // Se imprime la cantidad de notas inferiores al promedio
+        System.out.println(notasInferioresAPromedio);
 
     }
 
+    // Realizamos el metodo question2 Porcentaje de notas excelentes
     public static void question2() {
 
-        // Se contabiliza el número de notas inferiores al promedio de notas
-        int notas_excelentes = 0;
+        // Declaramos un contador de notas excelentes como double para despues hallar el
+        // porcentaje
+        double notasExcelentes = 0;
 
-        for (int i = 0; i < desempeño_de_estudiantes.size(); i++) {
-            // sumatoria_de_notas += desempeño_de_estudiantes.get(i).getCalificacion();
-            if (desempeño_de_estudiantes.get(i).getCalificacion() > 9) {
-                notas_excelentes += 1;
+        // Recorremos la lista desempeño de estudiantes para comparar el valor de notas
+        // excelente con cada una de las notas de la lista
+        for (int i = 0; i < desempeñoDeEstudiantes.size(); i++) {
+            // Si la nota es superior al valor de 9 la contamos en nuestro contador notas
+            // excelentes
+            if (desempeñoDeEstudiantes.get(i).getCalificacion() > 9) {
+                notasExcelentes += 1;
             }
         }
 
-        double b = notas_excelentes;
-        double c = desempeño_de_estudiantes.size();
+        // Cambiamos el tamaño de el tamaño de la lista desempeño de estudiantes a
+        // double para que no de error en la division
+        double c = desempeñoDeEstudiantes.size();
 
-        Double porcentaje = (b) / c;
+        // Calculamos el porcentaje
+        Double porcentaje = notasExcelentes / c;
+        // Redondeamos a dos cifras decimales como lo solicita el ejercicio
         porcentaje = Math.round(porcentaje * 100) / 100d;
 
+        // Empanada para colocar 0.00 si el porcentaje da cero
         if (porcentaje == 0) {
             System.out.println("0.00");
         } else {
-            System.out.println("Porcentaje superior a excelente " + porcentaje);
+            // Se imprime el valor del porcentaje con dos cifras decimales
+            System.out.println(porcentaje);
         }
 
     }
 
+    // Realizamos el metodo question3 Definir la materia con mayor numero de
+    // examenes reprobado
     public static void question3() {
 
-        // ¿Cuál es la materia con el mayor numero de examenes reprobados
+        // Declaramos las constantes Ingles y Historia - Constantes se declaran en Mayuscula Sostenida
+        final var INGLES = 1;
+        final var HISTORIA = 2;
+
         // Definimos los contadores para las materias: 1,2 y 3
         int materia_1 = 0;
         int materia_2 = 0;
         int materia_3 = 0;
 
-        for (int i = 0; i < desempeño_de_estudiantes.size(); i++) {
-            if (desempeño_de_estudiantes.get(i).getMateria() == 1) {
-                if (desempeño_de_estudiantes.get(i).getCalificacion() <= 6) {
+        // Recorremos la lista desempeño de estudiantes
+        for (int i = 0; i < desempeñoDeEstudiantes.size(); i++) {
+            // comparamos si la materia es idiomas
+            if (desempeñoDeEstudiantes.get(i).getMateria() == INGLES) {
+                // Comparamos si la nota esta inferior a 6
+                if (desempeñoDeEstudiantes.get(i).getCalificacion() <= 6) {
                     materia_1 += 1;
                 }
-            } else if (desempeño_de_estudiantes.get(i).getMateria() == 2) {
-                if (desempeño_de_estudiantes.get(i).getCalificacion() <= 6) {
+                // comparamos si la materia es historia
+            } else if (desempeñoDeEstudiantes.get(i).getMateria() == HISTORIA) {
+                // Comparamos si la nota esta inferior a 6
+                if (desempeñoDeEstudiantes.get(i).getCalificacion() <= 6) {
                     materia_2 += 1;
                 }
+                // comparamos si la materia es literatura
             } else {
-                if (desempeño_de_estudiantes.get(i).getCalificacion() <= 6) {
+                // Comparamos si la nota esta inferior a 6
+                if (desempeñoDeEstudiantes.get(i).getCalificacion() <= 6) {
                     materia_3 += 1;
                 }
             }
@@ -160,7 +213,7 @@ public class SchoolGradingSystem {
         // definir la materia con mayor número de examenes reprobados
         if (materia_1 >= materia_2 && materia_1 >= materia_3) {
             System.out.println("idiomas");
-        } else if (materia_2 > materia_3) {
+        } else if (materia_2 >= materia_3 && materia_2 > materia_1) {
             System.out.println("historia");
         } else {
             System.out.println("literatura");
@@ -168,21 +221,33 @@ public class SchoolGradingSystem {
 
     }
 
+    // Realizamos el metodo question4 ¿Cuál es el estudiante con el mejor desempeño
+    // para la materia idiomas?
     public static void question4() {
 
-        // ¿Cuál es el estudiante con el mejor desempeño para la materia idiomas?
-        float mayor_nota = 0f;
-        Float estudiante_mejor = 0f;
-        for (int i = 0; i < desempeño_de_estudiantes.size(); i++) {
-            if (desempeño_de_estudiantes.get(i).getMateria() == 1) {
-                if (desempeño_de_estudiantes.get(i).getCalificacion() > mayor_nota) {
-                    mayor_nota = desempeño_de_estudiantes.get(i).getCalificacion();
-                    estudiante_mejor = desempeño_de_estudiantes.get(i).getNombre();
+        // Declaramos las constante Ingles - Constantes se declaran en Mayuscula Sostenida
+        final var INGLES = 1;
+
+        // declaramos las variables mayor nota y mejor estudiante
+        float mayorNota = 0f;
+        Float estudianteMejor = 0f;
+
+        // Recorremos la lista desempeño de estudiantes
+        for (int i = 0; i < desempeñoDeEstudiantes.size(); i++) {
+            // Buscamos las notas de la materia ingles
+            if (desempeñoDeEstudiantes.get(i).getMateria() == INGLES) {
+                // Buscamos la nota mayor
+                if (desempeñoDeEstudiantes.get(i).getCalificacion() > mayorNota) {
+                    // Asignamos el dato a la mayor nota y al numero del mejor estudiante
+                    mayorNota = desempeñoDeEstudiantes.get(i).getCalificacion();
+                    estudianteMejor = desempeñoDeEstudiantes.get(i).getNombre();
                 }
             }
         }
+        // inicializamos la variable nombre estudiante
         var nombreestudiante = "";
-        switch (Math.round(estudiante_mejor)) {
+        // Usamos el metodo Switch para buscar el mejor estudiante
+        switch (Math.round(estudianteMejor)) {
             // Seleccionamos el caso que se debe ejecutar
             case 1:
                 nombreestudiante = "armando";
@@ -203,6 +268,7 @@ public class SchoolGradingSystem {
                 nombreestudiante = "alexandra";
                 break;
         }
+        // Imprimimos el nombre del mejor estudiante
         System.out.println(nombreestudiante);
 
     }
