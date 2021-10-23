3a958ea7737720c19ce5257d90328205cd4c5298
Willy Corzo Lubo <82851253+willyto3@users.noreply.github.com>
2021-10-23 12:14:20 -0500

    Clase usuario

--- PaginasWeb/PanaderiaconFlask/app/models.py
+++ PaginasWeb/PanaderiaconFlask/app/models.py
@@ -15,3 +15,15 @@ class Personas(db.Model):
     fechanacimiento = db.Column(db.String(200))
     contrasena = db.Column(db.String(200), nullable=True)
     vercontrasena = db.Column(db.String(200), nullable=True)
+
+class Usuarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(200), nullable=True)
+    apellido = db.Column(db.String(200), nullable=True)
+    direccion = db.Column(db.String(200))
+    celular = db.Column(db.Integer, nullable=True)
+    email = db.Column(db.String(200), nullable=True)
+    fechanacimiento = db.Column(db.String(200))
+    contrasena = db.Column(db.String(200), nullable=True)
+    vercontrasena = db.Column(db.String(200), nullable=True)
+    tipo = db.Column(db.String(200), nullable=True)
